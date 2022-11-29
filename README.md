8d4e9a557b8c214f9a9874ea006cb5a8cb7f55a5
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2022-11-29 21:31:43 +0000

    README file complete

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Password-Generator
 
+A password generator which asks the user how many characters the user needs, then it takes if the user wants lower case characters, upper case characters, numeric characters, or special characters. 
+
+It sadly however comes with `[object HTMLTextAreaElement]` in front of the password and nothing I have tried manages to fix it.
+
+![Screenshot](./starter/PasswordGenerator.png)
+
+[Password Generator](https://liamljhoward.github.io/Password-Generator/)
+
--- starter/script.js
+++ starter/script.js
@@ -1,134 +1,137 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-
-
 // Function to generate password with user input
-function generatePassword(){
-      pwdLength = parseInt(prompt(`How long would you like your password to be? Min 10 characters`)), 
-      ifLowerCase = confirm(`Would you like your password to include lower case letters?`),
-      ifUpperCase = confirm(`Would you like your password to include upper case letters?`),
-      ifNumeric = confirm(`Would you like your password to include numbers?`),
-      ifSpecialChar = confirm(`Would you like your password to include special characters? eg $@%&`)
-      console.log(pwdLength, ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar);
-      getRandom(ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar);
-     }
+function generatePassword() {
+  (pwdLength = parseInt(
+    prompt(`How long would you like your password to be? Min 10 characters`)
+  )),
+    (ifLowerCase = confirm(
+      `Would you like your password to include lower case letters?`
+    )),
+    (ifUpperCase = confirm(
+      `Would you like your password to include upper case letters?`
+    )),
+    (ifNumeric = confirm(`Would you like your password to include numbers?`)),
+    (ifSpecialChar = confirm(
+      `Would you like your password to include special characters? eg $@%&`
+    ));
+  console.log(pwdLength, ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar);
+  getRandom(ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar);
+}
 
 // Function for getting a random element from an array
-function getRandom(ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar){
-    const availableChars = [
+function getRandom(ifLowerCase, ifUpperCase, ifNumeric, ifSpecialChar) {
+  const availableChars = [
     ...(ifLowerCase ? lowerCasedCharacters : []),
     ...(ifUpperCase ? upperCasedCharacters : []),
     ...(ifNumeric ? numericCharacters : []),
     ...(ifSpecialChar ? specialCharacters : []),
   ];
-  for(let i = 0; i < pwdLength; i++){
+  for (let i = 0; i < pwdLength; i++) {
     let randomIndex = Math.floor(Math.random() * availableChars.length);
     console.log(randomIndex);
-      password += availableChars[randomIndex];
-    
+    password += availableChars[randomIndex];
   }
-  
+
   alert(password);
   return password;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -136,4 +139,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
